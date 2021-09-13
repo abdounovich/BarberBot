e6337c98be98565504fe2aa502d751eb7b21d78c
bob <abdounovich@gmail.com>
2021-09-13 20:21:55 +0200

    comment

--- database/migrations/2015_08_06_184708_create_settings_table.php
+++ database/migrations/2015_08_06_184708_create_settings_table.php
@@ -13,6 +13,7 @@ class CreateSettingsTable extends Migration
     public function up()
     {
         Schema::create('settings', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('key');
             $table->string('value')->nullable();
             $table->string('locale')->nullable();
