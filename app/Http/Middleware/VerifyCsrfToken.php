345317dbdff29026c7678027fd699eef2ab4983f
bob <abdounovich@gmail.com>
2021-09-13 23:53:16 +0200

    comment

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'botman',
+        'botman','https://barber-bot-dz.herokuapp.com/take_appointment',
     ];
 }
