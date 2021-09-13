31bad169891208322cc99f9214d5a5f7088b5599
bob <abdounovich@gmail.com>
2021-09-13 23:01:00 +0200

    comment

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -21,8 +21,10 @@
     <script src="{{URL::asset('js/jquery.throttle.js',true)}}"></script>
     <link href="{{URL::asset('css/jquery.classycountdown.css',true)}}" rel="stylesheet">
 <script src="{{URL::asset('js/jquery.classycountdown.js',true)}}"></script>
-@include('layouts.style.custom-style')
-
+     
+@php
+$image=Setting::get('theme.bg-image')
+@endphp
 <style>
   .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
@@ -60,6 +62,52 @@ button:hover, a:hover {
 }
 
 
+body{
+    
+  
+    background:url({{$image}}) ;
+font-family: 'Cairo', sans-serif;
+background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+
+}
+    .bg-premier{
+background-color:{{Setting::get('theme.premier')}};
+
+    }
+
+
+   
+    .btn-premier{
+      background-color:{{Setting::get('theme.premier')}};
+    }
+ 
+
+
+    .bg-deuxieme{
+background-color:{{Setting::get('theme.deuxieme')}};
+
+    }
+
+
+  
+    .btn-deuxieme{
+      background-color:{{Setting::get('theme.deuxieme')}};
+    
+    }
+ 
+    .text-color{
+      color:{{Setting::get('theme.text-color')}};
+    }
+ 
+ 
+
+    .text-premier{
+      color:{{Setting::get('theme.premier')}};
+    }
+ 
+ 
 
 </style>
 
