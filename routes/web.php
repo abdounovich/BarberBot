25cdea979b4f60ea5e8565c1fe0f69c08cc3ca70
bob <abdounovich@gmail.com>
2021-09-19 14:48:21 +0200

    comment

--- routes/web.php
+++ routes/web.php
@@ -61,13 +61,6 @@ Route::get('/parametres','SettingController@index')
 Route::get('/confirmationMessage/{id}/{debut}/{type}/{jour}/{username}/{Cid}','AppointmentController@confirmationMessage')
 ;
 
-Route::get('/t',function(){
-    echo "ok";
-    echo  "<script type='text/javascript'>";
-    echo "window.open(location, '_self').close();";
-    echo "</script>";
-
-});
 
 Route::get('/reminder','AppointmentController@reminder')
 ;
