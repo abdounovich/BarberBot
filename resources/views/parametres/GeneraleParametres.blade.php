8d3be9a703160a3d8dbd8330c0b55ec0cb059e3b
bob <abdounovich@gmail.com>
2021-09-21 20:14:59 +0200

    comment

--- resources/views/parametres/GeneraleParametres.blade.php
+++ resources/views/parametres/GeneraleParametres.blade.php
@@ -65,9 +65,9 @@ alt="" width="200" height="200">
 
 
     <button style="" type="submit" class="btn btn-success">  حفظ التعديلات </button>
-    <div class="form-group">
+    <div class="form-group mt-4">
         <div class="progress">
-            <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
+            <div class="progress-bar  progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
         </div>
     </div>
 </form>
@@ -91,7 +91,7 @@ alt="" width="200" height="200">
                 complete: function (xhr) {
                     console.log('File has uploaded');
                     $('#myForm').submit();
-                    
+
                 }
             });
         });
