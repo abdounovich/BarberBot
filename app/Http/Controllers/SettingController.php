1a8eda158746d990dada4f43a8c7a448abbe8787
bob <abdounovich@gmail.com>
2021-09-21 20:19:42 +0200

    comment

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -115,6 +115,7 @@ else{            $bg_image=$request->get('image');
             'bg-image' => $bg_image,
                 ]);
 
+                return response()->json(['success'=>'Successfully uploaded.']);
 
                 return view("/parametres")->with("success"," لقد تم حفظ البيانات بنجاح");
 
