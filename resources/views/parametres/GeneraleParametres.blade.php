ebd8fc75677406e664ea5375a20d14f6eb2bfc03
bob <abdounovich@gmail.com>
2021-09-21 20:20:07 +0200

    comment

--- resources/views/parametres/GeneraleParametres.blade.php
+++ resources/views/parametres/GeneraleParametres.blade.php
@@ -91,6 +91,7 @@ alt="" width="200" height="200">
                 complete: function (xhr) {
                     console.log('File has uploaded');
                     $('#myForm').submit();
+                    var percentage = '0';
 
                 }
             });
