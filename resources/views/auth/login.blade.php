b3aaf7a7477c0d6bb5e1c115df873278deaebf9f
bob <abdounovich@gmail.com>
2021-09-21 19:16:21 +0200

    comment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container  " style="opacity: 0.8" dir="rtl">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card bg-dark text-white">
+                <div class="card-header h4 bg-premier text-dark  text-center">{{ __('تسجيل الدخول') }}</div>
 
-                <div class="card-body">
+                <div class="card-body text-right">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('البريد الإلكتروني') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('كلمة المرور') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -39,22 +39,22 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row" >
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                    <label class="form-check-label mr-4" for="remember">
+                                        {{ __(' تذكرني') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-premier">
-                                    {{ __('Login') }}
+                        <div class="form-group row mb-0 text-center ">
+                            <div class="col-md-12 offset-md-12">
+                                <button type="submit" class="btn btn-premier text-center" style="width: 100%">
+                                    {{ __('دخـــــول') }}
                                 </button>
 
                             
--- resources/views/parametres/PrivateAppointment.blade.php
+++ resources/views/parametres/PrivateAppointment.blade.php
@@ -1,7 +1,7 @@
  <h3 class="text-center bg-premier p-4 rounded text-dark mt-4">حجز موعد خاص</h3>
 
 
-<div class="text-right bg-dark p-2" >
+<div class="text-right bg-dark p-2" style="opacity: 0.9" >
 
 
 
